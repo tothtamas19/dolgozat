2c8cbe1f768b16c5542da7d490151887c7ddcf11
tothtamas19 <90614062+tothtamas19@users.noreply.github.com>
2024-03-27 09:51:50 +0100

    Add files via upload

---
+++ elsoF/elsoF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Tracing;
+
+class Szemely
+{
+    public string nev;
+    public int eletkor;
+    public string nem;
+    public string lakhely;
+
+
+    static void Main(string[] args)
+    {
+        Szemely e = new Szemely();
+
+        e.nev = "Tomi";
+        e.eletkor = 22;
+        e.nem = "ferfi";
+        e.lakhely = "Viador utca 11, 1035";
+
+        Console.WriteLine(e.nev);
+        Console.WriteLine(e.eletkor);
+        Console.WriteLine(e.nem);
+        Console.WriteLine(e.lakhely);
+    }
+}
